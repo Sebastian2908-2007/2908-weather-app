7856dfc6a32d3602837b851166b6dfd2e8d15820
sebastian <sebastian bowen>
2021-03-05 21:01:22 -0700

    got cities appearing in searched cities and local storage IM actually doing ITTTgit add -A!

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,29 @@
  var showName = document.querySelector(".input-value-nm") 
  var desc = document.querySelector(".desc") 
  var temp = document.querySelector(".temp") 
+ var locale= [];
+ function loading() {
+  JSON.parse(localStorage.getItem("history"))
+  loading();
+}
+
 
  inpBtn.addEventListener("click", function() {
      // daily weather api url per city
    event.preventDefault();
-  fetch( "https://api.openweathermap.org/data/2.5/weather?q="+inputSearch.value+"&appid=c5c96daed38db003f5a5228c5b83c909")
+  fetch( "https://api.openweathermap.org/data/2.5/weather?q="+inputSearch.value+"&appid=c5c96daed38db003f5a5228c5b83c909&units=imperial")
   .then(function(response) {
     if (response.ok) {
       response.json().then(function(data) {
-        console.log(data);
+      var temperature = data.main.temp;
+       var descValue =  data.weather[0].description;
+       var nameCity = data.name;
+      localeStorage();
+      createCityBtn ();
+      showName.innerHTML = nameCity;
+      desc.innerHTML = descValue;
+      temp.innerHTML = temperature;
+      
       })
     }else {
       console.log("you can do this try again!");
@@ -20,3 +34,18 @@
   })
 
 })
+function localeStorage () {
+let save = inputSearch.value;
+locale.push(save)
+console.log(locale);
+localStorage.setItem("history", JSON.stringify(locale));
+}
+
+function createCityBtn () {
+  var container = document.querySelector(".gen");
+  var btnGen = document.createElement("button");
+  btnGen.innerHTML = inputSearch.value;
+  btnGen.className = "btn"
+  container.appendChild(btnGen);
+}
+
