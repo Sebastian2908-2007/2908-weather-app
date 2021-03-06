69fd274226fef88b8e60c400ac92426d8a6e6f53
sebastian <sebastian bowen>
2021-03-06 13:58:11 -0700

    have five day temps displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
  var inpBtn = document.querySelector('.inpBtn');
  var inputSearch = document.querySelector('.inputValue');
+ var fiveDaySearch = document.querySelector(".inputFiveDay");
+ var fveDayBtn = document.querySelector(".fveDay")
+ var fveDayName = document.querySelector(".input-value-fd")
+ var fveDayWeather = document.querySelector(".one")
+ var fveDayWeather1 = document.querySelector(".two")
+ var fveDayWeather2 = document.querySelector(".three")
+ var fveDayWeather3 = document.querySelector(".four")
+ var fveDayWeather4 = document.querySelector(".five")
+
+
+
+
+
  var showName = document.querySelector(".input-value-nm") 
  var desc = document.querySelector(".desc") 
  var temp = document.querySelector(".temp") 
@@ -48,4 +61,31 @@ function createCityBtn () {
   btnGen.className = "btn"
   container.appendChild(btnGen);
 }
+
+fveDayBtn.addEventListener("click", function () {
+  event.preventDefault();
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q="+fiveDaySearch.value+"&appid=c5c96daed38db003f5a5228c5b83c909&units=imperial")
+  .then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+          console.log(data);
+          var cityName = data.city.name;
+          var fiveDayDis = data.list[0].main.temp;
+          var fiveDayDis1 = data.list[1].main.temp;
+          var fiveDayDis2 = data.list[2].main.temp;
+          var fiveDayDis3 = data.list[3].main.temp;
+          var fiveDayDis4 = data.list[4].main.temp;
+          localeStorage();
+          fveDayName.innerHTML = cityName;
+          fveDayWeather.innerHTML = fiveDayDis;
+          fveDayWeather1.innerHTML = fiveDayDis1;
+          fveDayWeather2.innerHTML = fiveDayDis2;
+          fveDayWeather3.innerHTML = fiveDayDis3;
+          fveDayWeather4.innerHTML = fiveDayDis4;
+
+      })
+    }
+
+  })
+})
 
